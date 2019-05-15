cee5600c3ebf91efd39cd3a9f5d8624f8a291799
王强 <ActStrady@tom.com>
2019-05-15 16:23:44 +0800

    Initial commit

---
+++ service/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# @Time : 2019/5/15 16:14
+# @Author : ActStrady@tom.com
+# @FileName : __init__.py.py
+# @GitHub : https://github.com/ActStrady/selenium-learn
